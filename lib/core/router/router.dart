dbde8015908ab9ad10f8cef4895cadbb9d2869f5
abdulkash <smile.kash.abdul@gmail.com>
2024-11-19 11:42:24 +0100

    setup authpage

--- lib/core/router/router.dart
+++ lib/core/router/router.dart
@@ -1,7 +1,34 @@
+import 'dart:developer';
+
 import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+import 'package:prelura_app/core/router/router.gr.dart';
 
 @AutoRouterConfig()
 class AppRouter extends RootStackRouter {
   @override
-  List<AutoRoute> get routes => [];
+  List<AutoRoute> get routes => [
+        AutoRoute(page: AuthRoute.page, initial: true),
+      ];
+}
+
+/// Route Observer for ADrop [AppRouter] to log events for [didPush]
+/// to better debug routing
+class AppRouterObserver extends AutoRouterObserver {
+  @override
+  void didPush(Route<dynamic> route, Route<dynamic>? previousRoute) {
+    if (route.settings.name == null) return;
+    log('Pushed ${route.settings.name}', name: 'RouteObserver');
+  }
+
+  @override
+  void didPop(Route<dynamic> route, Route<dynamic>? previousRoute) {
+    if (route.settings.name == null) return;
+    log('Popped ${route.settings.name}', name: 'RouteObserver');
+  }
+
+  @override
+  void didChangeTabRoute(TabPageRoute route, TabPageRoute previousRoute) {
+    log('Switched Tab ${route.name}', name: 'RouteObserver');
+  }
 }
---
+++ lib/core/router/router.gr.dart
@@ -0,0 +1,31 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// **************************************************************************
+// AutoRouterGenerator
+// **************************************************************************
+
+// ignore_for_file: type=lint
+// coverage:ignore-file
+
+// ignore_for_file: no_leading_underscores_for_library_prefixes
+import 'package:auto_route/auto_route.dart' as _i2;
+import 'package:prelura_app/modules/auth/views/pages/auth_page.dart' as _i1;
+
+/// generated route for
+/// [_i1.AuthPage]
+class AuthRoute extends _i2.PageRouteInfo<void> {
+  const AuthRoute({List<_i2.PageRouteInfo>? children})
+      : super(
+          AuthRoute.name,
+          initialChildren: children,
+        );
+
+  static const String name = 'AuthRoute';
+
+  static _i2.PageInfo page = _i2.PageInfo(
+    name,
+    builder: (data) {
+      return const _i1.AuthPage();
+    },
+  );
+}
