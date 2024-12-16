ee32e5360ff51581e9502beaa334c042ab4a6cad
abdulkash <smile.kash.abdul@gmail.com>
2024-12-16 11:22:07 +0100

    update

--- lib/modules/views/pages/product detail/widget/product_description.dart
+++ lib/modules/views/pages/product detail/widget/product_description.dart
@@ -49,36 +49,24 @@ class ProductDescription extends ConsumerWidget {
                 ),
                 GestureDetector(
                   onTap: () {
-                    ref.read(isDescriptionExpandedProvider.notifier).state =
-                        !isDescriptionExpanded;
+                    ref.read(isDescriptionExpandedProvider.notifier).state = !isDescriptionExpanded;
                   },
                   child: Text.rich(
                     TextSpan(
                       children: [
                         TextSpan(
-                          text: isDescriptionExpanded
-                              ? product.description
-                              : truncatedDescription,
+                          text: isDescriptionExpanded ? product.description : truncatedDescription,
                           style: Theme.of(context).textTheme.bodyMedium,
                         ),
                         ...buildHashtagText(
-                          isDescriptionExpanded
-                              ? product.description
-                              : truncatedDescription,
+                          isDescriptionExpanded ? product.description : truncatedDescription,
                           Theme.of(context).textTheme.bodyMedium,
-                          Theme.of(context).textTheme.bodyMedium?.copyWith(
-                              color: PreluraColors.activeColor,
-                              fontWeight: FontWeight.bold),
+                          Theme.of(context).textTheme.bodyMedium?.copyWith(color: PreluraColors.activeColor, fontWeight: FontWeight.bold),
                         ),
                         if (shouldShowSeeMore)
                           TextSpan(
-                            text: isDescriptionExpanded
-                                ? " See less"
-                                : " See more",
-                            style: Theme.of(context)
-                                .textTheme
-                                .bodyMedium
-                                ?.copyWith(
+                            text: isDescriptionExpanded ? " See less" : " See more",
+                            style: Theme.of(context).textTheme.bodyMedium?.copyWith(
                                   color: Colors.blue,
                                   fontWeight: FontWeight.bold,
                                 ),
@@ -96,14 +84,9 @@ class ProductDescription extends ConsumerWidget {
             height: 2,
             thickness: 1,
           ),
-          if (product.category != null)
-            _buildInfoRow("Category", product.category!.name, context),
-          if (product.subCategory != null)
-            _buildInfoRow("Sub Category", product.subCategory!.name, context),
-          if (product.style != null)
-            _buildInfoRow(
-                "Style", product.style!.name.replaceAll("_", " "), context,
-                style: context.textTheme.labelLarge),
+          if (product.category != null) _buildInfoRow("Category", product.category!.name, context),
+          if (product.subCategory != null) _buildInfoRow("Sub Category", product.subCategory!.name, context),
+          if (product.style != null) _buildInfoRow("Style", product.style!.name.replaceAll("_", " ").toLowerCase(), context, style: context.textTheme.labelLarge),
           if (product.materials != null)
             if (product.materials!.isNotEmpty)
               _buildInfoRow(
@@ -112,14 +95,10 @@ class ProductDescription extends ConsumerWidget {
                 context,
               ),
 
-          if (product.size != null)
-            _buildInfoRow(
-                "Size", product.size!.name.replaceAll('_', ' '), context),
-          if (product.condition != null)
-            _buildInfoRow("Condition", product.condition!.simpleName, context),
+          if (product.size != null) _buildInfoRow("Size", product.size!.name.replaceAll('_', ' '), context),
+          if (product.condition != null) _buildInfoRow("Condition", product.condition!.simpleName, context),
           _buildInfoRow("Views", product.views.toString(), context),
-          _buildInfoRow("Uploaded",
-              DateFormat.yMMMMEEEEd().format(product.createdAt), context),
+          _buildInfoRow("Uploaded", DateFormat.yMMMMEEEEd().format(product.createdAt), context),
           Container(
             padding: const EdgeInsets.symmetric(vertical: 22.0, horizontal: 16),
             decoration: BoxDecoration(
@@ -140,8 +119,7 @@ class ProductDescription extends ConsumerWidget {
                 ),
                 Text(
                   "Postage: From £${dummy.postageCost.toStringAsFixed(2)}",
-                  style: Theme.of(context).textTheme.bodyMedium?.copyWith(
-                      fontWeight: FontWeight.w500, color: Colors.purple),
+                  style: Theme.of(context).textTheme.bodyMedium?.copyWith(fontWeight: FontWeight.w500, color: Colors.purple),
                 ),
               ],
             ),
@@ -254,8 +232,7 @@ class ProductDescription extends ConsumerWidget {
     return description;
   }
 
-  Widget _buildInfoRow(String label, String value, context,
-      {TextStyle? style}) {
+  Widget _buildInfoRow(String label, String value, context, {TextStyle? style}) {
     return Container(
       padding: const EdgeInsets.symmetric(vertical: 22.0, horizontal: 16),
       decoration: BoxDecoration(
@@ -276,11 +253,7 @@ class ProductDescription extends ConsumerWidget {
           ),
           Text(
             value,
-            style: style ??
-                Theme.of(context)
-                    .textTheme
-                    .bodyMedium
-                    ?.copyWith(color: PreluraColors.greyColor),
+            style: style ?? Theme.of(context).textTheme.bodyMedium?.copyWith(color: PreluraColors.greyColor),
           ),
         ],
       ),
