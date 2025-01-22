055dc452aef52953b5f1db784cbe8bb959f98c14
AYOPELUMI <ayopelumi2014@gmail.com>
2025-01-22 09:30:07 -0800

    Wednesday tasks 22th-1-25

--- lib/controller/product/product_provider.dart
+++ lib/controller/product/product_provider.dart
@@ -161,8 +161,10 @@ final userProduct =
   //     .firstOrNull;
   // final size =
   //     Enum$SizeEnum.values.where((e) => e.name == sizeFilter).firstOrNull;
-  final condition =
-      ConditionsEnum.values.where((e) => e.name == conditionFilter).firstOrNull;
+  final condition = ConditionsEnum.values
+      .where(
+          (e) => e.simpleName.toLowerCase() == conditionFilter?.toLowerCase())
+      .firstOrNull;
   final style =
       Enum$StyleEnum.values.where((e) => e.name == styleFilter).firstOrNull;
   final parentCategory = Enum$ParentCategoryEnum.values
@@ -177,8 +179,8 @@ final userProduct =
 
   final maxPrice = priceFilter?.split(' ').last;
   final minPrice = priceFilter?.split(' ').first;
-  log(maxPrice.toString(), name: 'maxPrice');
-  log(minPrice.toString(), name: 'minPrice');
+  log(condition.toString(), name: 'condition');
+  log(conditionFilter.toString(), name: 'condition Filter');
 
   final searchQuery = ref.watch(userProductSearchQuery);
   final sort = ref.watch(userProductSort);
