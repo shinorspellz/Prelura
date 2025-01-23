ce81f815d8a011f5fc43687d652adcaeaec3176d
JidNation <jaid1074@gmail.com>
2025-01-23 11:53:10 +0100

    done: first offer stage UI

--- lib/views/pages/chat/widgets/offer_conversation_builder.dart
+++ lib/views/pages/chat/widgets/offer_conversation_builder.dart
@@ -5,12 +5,10 @@ import 'package:prelura_app/controller/user/user_controller.dart';
 import 'package:prelura_app/model/chat/conversation_model.dart';
 import 'package:prelura_app/model/chat/offer_info.dart';
 import 'package:prelura_app/model/user/user_model.dart';
-import 'package:prelura_app/res/colors.dart';
-import 'package:prelura_app/res/username.dart';
 import 'package:prelura_app/views/widgets/app_button.dart';
 import 'package:prelura_app/views/widgets/gap.dart';
-import 'package:prelura_app/views/widgets/profile_picture.dart';
 
+import 'offer_card.dart';
 import 'offer_product_card.dart';
 import 'offer_subchild_box.dart';
 
@@ -96,53 +94,5 @@ class OfferConversationBuilder extends ConsumerWidget {
       //       },
       //     ),
     ]);
-  }
-}
-
-class OfferFirstCard extends ConsumerStatefulWidget {
-  const OfferFirstCard({
-    super.key,
-    required this.conversationInfo,
-    required this.amTheSeller,
-    required this.isSender,
-  });
-
-  final ConversationModel conversationInfo;
-  final bool amTheSeller;
-  final bool isSender;
-
-  @override
-  ConsumerState<OfferFirstCard> createState() => _OfferFirstCardState();
-}
-
-class _OfferFirstCardState extends ConsumerState<OfferFirstCard> {
-  @override
-  Widget build(BuildContext context) {
-    final OfferInfo offerInfo = widget.conversationInfo.offer!;
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
-      child: Row(
-          mainAxisAlignment:
-              widget.isSender ? MainAxisAlignment.end : MainAxisAlignment.start,
-          children: [
-            if (widget.amTheSeller)
-              ProfilePictureWidget(
-                height: 50,
-                width: 50,
-                profilePicture: "${offerInfo.buyer?.profilePictureUrl}",
-                username: offerInfo.buyer?.username,
-              ),
-            8.horizontalSpacing,
-            Column(children: [
-              HighlightUserName(
-                isRead: false,
-                highlightColor: PreluraColors.primaryColor,
-                message:
-                    "${widget.isSender ? "you" : offerInfo.buyer?.username} Offered £${offerInfo.offerPrice}",
-                username: "offered",
-              ),
-            ]),
-          ]),
-    );
   }
 }
