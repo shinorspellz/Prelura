04121c06f24b2f19adc888ac6f3a9bdf1460e8fa
JidNation <jaid1074@gmail.com>
2025-01-24 12:31:56 +0100

    wip: handling product count

--- lib/views/pages/chat/widgets/offer_subchild_box.dart
+++ lib/views/pages/chat/widgets/offer_subchild_box.dart
@@ -374,6 +374,26 @@ class OfferSubCardBoxState extends ConsumerState<OfferSubCardBox> {
         ),
       ],
       if (status == "accepted" && !amTheSeller) ...[
+        Padding(
+          padding: const EdgeInsets.only(
+            left: 20,
+            right: 20,
+            top: 20,
+          ),
+          child: Align(
+            alignment: Alignment.center,
+            child: Text(
+              "Payment made, please wait for the seller to send the item.",
+              textAlign: TextAlign.center,
+              style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                    color: PreluraColors.grey,
+                    fontSize: 12,
+                  ),
+            ),
+          ),
+        ),
+      ],
+      if (status == "accepted" && !amTheSeller) ...[
         // addVerticalSpacing(20),
         // Text(
         //   "Please wait for buyer to make payment",
